de068cb396a1c30fbee257ebd4bb7d4539b669ec
lily <att1667@naver.com>
2023-10-24 20:10:26 +0900

    Initial Commit

---
+++ FortuneCookie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FortuneCookie
+//
+//  Created by 최혜지 on 2023/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FortuneCookie/FortuneCookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneCookieApp.swift
+//  FortuneCookie
+//
+//  Created by 최혜지 on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FortuneCookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
